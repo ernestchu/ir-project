bcf0fc19efda6432318c4eaf6e1deb9d352c580c
jlee844 <jlee844@jh.edu>
2025-04-19 00:33:28 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,41 +1,68 @@
-import numpy as np
-import pandas as pd
-import os
-import math
-
-
-
-from datasets import load_dataset
-from datasets import load_from_disk
-import re
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer
-from nltk.tokenize import word_tokenize
 import json
-# nltk.download('punkt')
-# nltk.download('stopwords')
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from gensim.models import Word2Vec
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from sklearn import cosine_similarity
+import nltk
 
-ds = load_dataset("ernestchu/lrclib-20250319", "default")
+nltk.download("punkt_tab")
+nltk.download("stopwords")
+
+# === Load Data ===
+with open("train_split.json", "r") as f:
+    train_data = json.load(f)
+
+with open("test_split.json", "r") as f:
+    test_data = json.load(f)
+
+# tokenize 
+stop_words = set(stopwords.words("english"))
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    return tokens
+
+def remove_stopwords(tokens):
+    tokens = [w for w in tokens if w not in stop_words]
+
+# For word2vec 
+def preprocess_texts(texts):
+    return [" ".join(tokenize(t)) for t in texts]
 
 
-lyrics_data = ds["train"]
-print(len(lyrics_data))
-raw_lyrics = [example["plain_lyrics"] for example in lyrics_data if example["has_plain_lyrics"]]
-
-#load label:
-with open("emotion_labels.json", "r") as f:
-    label_dict = json.load(f)
-
-labels = []
-for i in range(len(raw_lyrics)):
-    key = str(i)
-    label = label_dict.get(key)
-    labels.append(label)
 
 
-stop_words = set(stopwords.words('english'))
-stemmer = SnowballStemmer("english")
+# TF-IDF 
+def tfidf_vectors(train_texts):
+    tfidf = TfidfVectorizer()
+    tfidf_train = tfidf.fit_transform(train_texts)
+    return tfidf_train
+
+# Word2Vec 
+#not done with this part yet need to think about how to further remove unmeaning full words before calling word2vec function 
+
+def word2vec(lyrics):
+    model = Word2Vec(lyrics)
+    return model
+
+
+
+#Bayesian Classifier
+
+
+
+
+
+
+
+#experiment pipeline
+
+tfidf_train = tfidf_vectors()
+word2vec_model = word2vec()
+
+#get tfidf for without stop word
 
 
 
